352ef9b09d0ff7e0957ef0b1edcc204751383304
Gautam Thampy <thampygautam@gmail.com>
2024-07-17 18:45:00 +0530

    added bottom nav bar and added animations

--- app/src/main/java/com/example/wakewisefinal/MainActivity.java
+++ app/src/main/java/com/example/wakewisefinal/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.wakewisefinal;
 
+import android.annotation.SuppressLint;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView mBottomNav = findViewById(R.id.navigation);
+
+        mBottomNav.setOnItemSelectedListener(item -> {
+            ((TextView)findViewById(R.id.temptext)).setText(item.getTitle().toString()+": Try scrolling m8");
+            return true;
+        });
     }
 }
